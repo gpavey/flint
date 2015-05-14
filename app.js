853b8958a25dffa2a54f4eed149ac9da734222b8
Geoff Pavey <geoff.pavey@gmail.com>
2015-05-14 14:29:28 -0700

    added individual product and contacts

--- app.js
+++ app.js
@@ -1,48 +1,78 @@
-'use strict';
-
 var App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
-App.Router.map(function(){
+App.Router.map(function() {
+  this.route('credits', { path: '/thanks' });
   this.route('about');
-  this.route('credits', {path:'/thanks'});
   this.resource('products');
+  this.resource('contacts');
+  this.resource('product', { path: '/products/:title' });
+  this.resource('contact', { path: '/contacts/:name' });
+
 });
 
 App.IndexController = Ember.Controller.extend({
-  productsCount: 10,
+  productsCount: 6,
   logo: 'images/logo-small.png',
-  time: function(){
-    return (new Date()).toDateString()
+  time: function() {
+    return (new Date()).toDateString();
   }.property()
 });
-
 App.AboutController = Ember.Controller.extend({
-  contactName: 'Geoff',
+  contactName: 'Anostagia',
   avatar: 'images/avatar.png',
-  open: function(){
-    return ((new Date()).getDay() === 0) ? 'Closed' : 'Open';
+  open: function() {
+    return ((new Date()).getDay() === 0) ? "Closed" : "Open";
   }.property()
 });
 
 App.ProductsRoute = Ember.Route.extend({
   model: function() {
-   return App.PRODUCTS;
+    return App.PRODUCTS;
   }
 });
+App.ProductRoute = Ember.Route.extend({
+  model: function(params){
+    return App.PRODUCTS.findBy('title', params.title);
+  }
+});
+App.ContactsRoute = Ember.Route.extend({
+  model: function() {
+    return App.CONTACTS;
+  }
+});
+App.ContactRoute = Ember.Route.extend({
+  model: function(params){
+    return App.CONTACTS.findBy('name', params.name);
+  }
+});
+
 App.PRODUCTS = [
   {
-    title: 'Guns',
-    price: 100,
-    description: 'You dont know what a gun is?',
+    title: 'Flint',
+    price: 99,
+    description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
     isOnSale: true,
     image: 'images/products/flint.png'
   },
   {
-    title: 'Butter',
-    price: 100,
-    description: 'I cant believe its not butter',
-    isOnSale: true,
+    title: 'Kindling',
+    price: 249,
+    description: 'Easily combustible small sticks or twigs used for starting a fire.',
+    isOnSale: false,
     image: 'images/products/kindling.png'
+  }
+];
+
+App.CONTACTS = [
+  {
+    name: 'Giamia',
+    about: 'Although Giamia came from a humble spark of lightning, he quickly grew to be a great craftsman, providing all the warming instruments needed by those close to him.',
+    avatar: 'images/contacts/giamia.png'
+  },
+  {
+    name: 'Anostagia',
+    about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
+    avatar: 'images/contacts/anostagia.png'
   }
 ];
